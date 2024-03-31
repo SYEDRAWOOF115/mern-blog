1b1bd9ad4583b661c81b3305ee67120edff171ce
syedrawoof <syedrawoof115@gmail.com>
2024-03-31 12:42:17 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
-import { log } from 'console';
-import express from 'express'
+import mongoose from 'mongoose';
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("mongodb is connected");
+}).catch((err)=>{
+    console.log(err);
+});
 const app=express();
 app.listen(3000, ()=>{
     console.log('server is running on port 3000');
